77d67a3b15fc06ea81510f5db1a997a44911704b
chris3309 <christopher@putzler.org>
2025-03-06 02:22:19 -0600

    Config.java and api key in flashcard
    Config.java gets the api from config.properties

--- Flashcard.java
+++ Flashcard.java
@@ -1,17 +1,24 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+
 public class Flashcard {
-    private String question;
-    private String answer;
+    private String ref;
+    
+    private static final String API_URL = "https://api.esv.org/v3/passage/text/";
+    private static final String API_KEY = Config.getApiKey();
 
-    public Flashcard(String question, String answer) {
-        this.question = question;
-        this.answer = answer;
+    public Flashcard(String ref) {
+        this.ref = ref;
     }
 
-    public String getQuestion() {
-        return question;
+    public String getReference() {
+        return ref;
     }
 
-    public String getAnswer() {
-        return answer;
-    }
 }
